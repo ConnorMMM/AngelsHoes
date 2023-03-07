6568ac32066127731ea39b8af60d48806bef72dc
Maddy Ellicott <maddyellicott195@gmail.com>
2023-03-07 15:46:46 +1100

    MENU SCRIPTS IMPLEMENTED AND COLOURS CHANGED

--- AngelsSacrifice/Assets/Scripts/MenuScripts/PauseMenu.cs
+++ AngelsSacrifice/Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -9,33 +9,14 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
     public GameObject PauseMenuUI; 
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
-    }
-
+    
     public void Pause()
     {
-        PauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsPaused = true;
+        LevelManager.Instance.PauseGame(true);
     }
 
     public void Resume()
     {
-        PauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
+        LevelManager.Instance.PauseGame(false);
     }
 }
