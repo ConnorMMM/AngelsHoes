b10aaedc2d5577520be0a278e26aa908e370f5f4
Luzlly <luzllytohi@gmail.com>
2023-03-08 10:12:25 +1100

    Resolved Merge Conflicts

--- AngelsSacrifice/Assets/Scripts/MenuScripts/PauseMenu.cs
+++ AngelsSacrifice/Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -6,13 +6,10 @@ using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-<<<<<<< HEAD
-=======
     public static bool GameIsPaused = false;
     public GameObject PauseMenuUI; 
 
     
->>>>>>> 91f33280766ead03e87fa80574b588760d52aa8f
     public void Pause()
     {
         LevelManager.Instance.PauseGame(true);
@@ -21,13 +18,10 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         LevelManager.Instance.PauseGame(false);
-<<<<<<< HEAD
     }
 
     public void ExitToMenu()
     {
         SceneManager.LoadScene(0);
-=======
->>>>>>> 91f33280766ead03e87fa80574b588760d52aa8f
     }
 }
